d1adf8b3051587a0274bb229ee9d155328e4d2dd
Julia Ayomide <145088749+juliaayomide@users.noreply.github.com>
2023-10-06 09:14:54 -0300

    exercicioD

---
+++ exd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+public class exD
+  public static void main(String args[]) {
+
+  Scanner in = new Scanner (System.in);
+  int num, fatorial-1, i=1
+  System.out.print("Qual numero deseja saber o fatorial?");
+  num= in.nextInt();
+  while (i <= num) {
+    fatorial = fatorial*i;
+    i++;
+}
+
+  System.out.println("O fatorial do numero é "+fatorial);
+  }
+}
