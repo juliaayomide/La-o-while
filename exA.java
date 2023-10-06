d74f6084f9cf1fad75bb1aa33f77bcdd37d98b66
Julia Ayomide <145088749+juliaayomide@users.noreply.github.com>
2023-10-06 08:55:14 -0300

    exercicioA

---
+++ exA.java
@@ -0,0 +1,26 @@
+public class exA {
+
+
+
+public static void main(String args[]) {
+
+
+
+    int i-1, pedro-145;
+
+    double joao-134;
+
+    while (joao<pedro) {
+  
+       joao Joao+2.5;
+
+       pedro pedro+2;
+       i++;
+
+       }
+
+System.out.println("levou "+i+" anos para João ultrapassar Pedro");
+tem menu de contexto
+   }
+
+}
