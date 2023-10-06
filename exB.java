8c7a4e6d5d9967597f4854b4dfb4970158901231
Julia Ayomide <145088749+juliaayomide@users.noreply.github.com>
2023-10-06 09:11:33 -0300

    exercicioB

---
+++ exB.java
@@ -0,0 +1,19 @@
+ import java.util.Scanner;
+ public class exB {
+public static void main(String args[]) {
+
+  Scanner in = new Scanner(System.in);
+  int n, proximotermo, ptermo = 0, stermo = 1, i=1;
+  System.out.println("Qual o numero");
+  n = in.nextInt();|
+  System.out.println("A serie da fibonacci até o "+n+"ésimo número é: ");
+  while(i<=n) t
+  System.out.println(ptermo+""); proximotermo = ptermo + stermo;
+  proximotermo = ptermo + stermo;
+  ptermo stermo;
+  stermo proximotermo;
+  i++;
+
+    }
+  }
+}
